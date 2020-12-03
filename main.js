9128cbfda635c4eb717abb64c835c1fb0f37774b
Aliza Howe <alizahowe26@gmail.com>
2020-12-03 14:45:49 -0500

    Draft of character list

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+// Character list is an array. The array represents a single team.
+// Characters themselves are objects. The object name will be the character's name, and each key/property unit will be a stat (e.g., HP: 100;)
+
 const characterList = [
-    'Billy',
-    'Bob',
-    'Joe'
+    char1 = {
+		name: "",
+		HP: 100,
+	},
+	char2 = {
+		name: "",
+		HP: 100,
+	},
+	char3 = {
+		name: "",
+		HP: 100,
+	},
+	char4 = {
+		name: "",
+		HP: 100,
+	},
+	char5 = {
+		name: "",
+		HP: 100,
+	},
+	char6 = {
+		name: "",
+		HP: 100,
+	}
 ];
 
-console.log(characterList);
+for (element of characterList) {
+	console.log(element);
+}
+
+// let team = [team array with chars]
+// When user selects a character, that character is added to *team* array
