4c8ee8a0795b4300d03e971acc3ffb3d3ebd8f5d
Aliza Howe <alizahowe26@gmail.com>
2020-12-09 13:29:53 -0500

    Chart works. Added some extra buttons to make a cleaner experience

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const team1Div = document.querySelector('#team1Div');
 const team2Div = document.querySelector('#team2Div');
+const chooseTeam1Button = document.querySelector("#selectTeam1");
+const chooseTeam2Button = document.querySelector("#selectTeam2");
+const startGameButton = document.querySelector("#startGame");
 
 // Character list is an array. The array represents a single team.
 // Characters themselves are objects. The object name will be the character's name, and each key/property unit will be a stat (e.g., HP: 100;)
+
 
 const characterList = [
     {
@@ -21,7 +25,7 @@ const characterList = [
 		speed: 75,
 	},
 	{
-		name: "Alfa",
+		name: "Alpha",
 		HP: 100,
 		speed: 75,
 	},
@@ -37,12 +41,8 @@ const characterList = [
 	}
 ];
 
-//for (element of characterList) {
-//	console.log(element);
-//};
-
-let team1 = [];
-let team2 = [];
+let team1;
+let team2;
 
 const chooseTeam = function() {
 
@@ -51,43 +51,64 @@ const chooseTeam = function() {
     for (let counter = 1; counter <= 3; counter++) {
         let choice = prompt("Choose your " + counter + " member!\nPick their number!");
 		choice--;
-        
-        let charArray = [];
-
-        charArray.push(characterList[choice].name);
-        charArray.push(characterList[choice].HP);
-        teamArray.push(charArray);
+        teamArray.push(characterList[choice]);
+		console.log(characterList[choice]);
     };
     return teamArray;
 };
 
-const appendTeam1 = function() {
-        let team = chooseTeam();
-        team1.push(team);
-    
-        for (let char of team) {
-            charDiv = document.createElement('p');
-            charDiv.textContent = char;
-            team1Div.appendChild(charDiv);
-        }
+const makeTeam1 = function() {
+	team1 = chooseTeam();
+	console.log("makeTeam1");
+    appendTeam(team1, team1Div);
+	chooseTeam1Button.remove();
+	return team1;
+}
+
+const makeTeam2 = function() {
+	team2 = chooseTeam();
+	console.log("makeTeam2");
+    appendTeam(team2, team2Div);
+	chooseTeam2Button.remove();
+	return team2;
+}
+
+chooseTeam1Button.addEventListener("click", makeTeam1);
+chooseTeam2Button.addEventListener("click", makeTeam2);
+
+// Make a div for each character INSIDE the teamDiv.
+// Have sub-divs inside character divs; character name is on the left, and character HP is on the right.
+// Add the entire character div into the teamDiv.
+const appendTeam = function(team, teamDiv) {
+    for (let char of team) {
+		let charDiv = document.createElement("div");
+        let nameDiv = document.createElement("div");
+		nameDiv.innerHTML = char.name;
+        charDiv.appendChild(nameDiv);
+		let healthDiv = document.createElement("div");
+		healthDiv.innerHTML = char.HP;
+		charDiv.appendChild(healthDiv);
+		teamDiv.appendChild(charDiv);
+    }
+		console.log("appendTeam");
     };
 
-const appendTeam2 = function() {
-        let team = chooseTeam();
-        team2.push(team);
-    
-        for (let char of team) {
-            charDiv = document.createElement('p');
-            charDiv.textContent = char;
-            team2Div.appendChild(charDiv);
-        }
-    };
-
-const makeTeams = function() {
-    appendTeam1();
-    appendTeam2();
-    let button = document.querySelector('#selectTeams');
-    button.remove();
+const startGame = function() {
+    console.log("This is where the game starts");
 };
 
-// When user selects a character, that character is added to *team* array
+startGameButton.addEventListener("click", startGame);
+
+// function for subtracting a character's HP due to an attack
+const attackDamage = function(attackStrength, target) {
+	let originalHP = target.HP; // let originalHP = target.HP;
+	if (originalHP > attackStrength) {
+		target.HP -= attackStrength;
+		console.log(target.name + "'s HP went from " + originalHP + " to " + target.HP + "!"); // display on page instead of doing console.log()
+	} else if (originalHP <= attackStrength) {
+		target.HP = 0; // so HP can't go into the negatives
+		console.log(target.name + " is defeated!"); // display on page instead of doing console.log()
+	}
+};
+
+//attackDamage(30, team1);
